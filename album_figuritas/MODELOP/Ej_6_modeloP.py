9a56fdcd1aee211d407668e59c97309e6c8f470e
Meliattar <meliattarcohen@gmail.com>
2022-05-11 10:34:54 -0300

    TERMINADO PRELIMINAR

--- album_figuritas/MODELOP/Ej_6_modeloP.py
+++ album_figuritas/MODELOP/Ej_6_modeloP.py
@@ -4,15 +4,13 @@ figus_total = 6
 
 def comprar_figu(figus_total):
         figurita_obtenida = random.randint(0,figus_total-1)
-        print(figurita_obtenida)
+        #print(figurita_obtenida)
         return figurita_obtenida
         
-
 def esta_lleno(album):
     if 0 in album:
         return False
     return True  
-
 
 def cuantas_figus(figus_total):
     album = [0]*figus_total
@@ -20,25 +18,25 @@ def cuantas_figus(figus_total):
     while esta_lleno(album) == False:
         album[comprar_figu(figus_total)] = 1
         completas += 1
-        print(album)
-    print(esta_lleno(album))
+    #print(album)
+    #print(esta_lleno(album))
     
     return completas
 
 lista = [1,2,3,4,5,6,7,8,9,10]
 def funcion_promedio(lista):
     promedio = sum(lista)/len(lista) 
-    print(promedio)
+    #print(promedio)
 
 funcion_promedio(lista)
-print(cuantas_figus(figus_total)) #poner texto de "Tuviste que comprar tantas figus para llenar el album"
+cuantas_figus(figus_total) #poner texto de "Tuviste que comprar tantas figus para llenar el album"
 
 def funcion_5 ():
     promedios = []
-    n_repeticiones = 1000
+    n_repeticiones = 100
     i=0
     while i < n_repeticiones:
-        promedios.append(cuantas_figus(6))
+        promedios.append(cuantas_figus(669))
         i += 1
     print(funcion_promedio(promedios))
 funcion_5()
---
+++ album_figuritas/info.txt
@@ -0,0 +1,21 @@
+#699 figuritas
+#cada figurita se imprime en cantidades iguales
+#se distribuye random
+#los paquetes son de 5 figuritas
+
+#Biblioteca Numpy --> es una biblioteca (paquetes con funciones de matematica)
+#Instalacion Numpy --> pip install numpy --> por red cableada
+#import random
+#import numpy as np
+
+#FUNCIONES UTILES:
+#random.radint(a,b) --> genera un numero entero aleatorio entre a y b inclusive
+#np.zeros(a) --> nos da un array (NO LISTA) de llena de ceros de tamaño a.
+#Tambien con listas se puede hacer --> [0]* (se arma una lista de ceros x el numero que lo multiplico ej: [0]*4 lista=[0,0,0,0])
+
+#OBSERVACIONES
+#Los arrays no tienen .append ni .pop
+
+#OPERACIONES
+#lista del album --> [0]*699
+#cambiar/agregar elemento--> lista[posicion en la que quiero agregar].append 
