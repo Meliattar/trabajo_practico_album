302c76ebf19d78e979d6cb05d463bc5e6d4e0896
Meliattar <meliattarcohen@gmail.com>
2022-05-21 13:23:17 -0300

    Update Ej_4_modeloA.py

--- album_figuritas/MODELOA/Ej_4_modeloA.py
+++ album_figuritas/MODELOA/Ej_4_modeloA.py
@@ -2,13 +2,11 @@ import numpy as np
 import random 
 
 figus_paquete = 0
-#figus_total = int(input("Ingrese cuantas figuritas tiene su album en total: "))
 figus_total =669
 def generar_paquete(figus_total, figus_paquete):
     while figus_paquete <= 5: 
         figurita_obtenida = random.randint(0,figus_total-1)
         figus_paquete += 1
-        #print(figurita_obtenida)
         return figurita_obtenida
 
 def esta_lleno(album):
@@ -21,19 +19,19 @@ def cuantos_paquetes(figus_total, figus_paquete):
     paquetes = 0
     while esta_lleno(album) == False:
         album[generar_paquete(figus_total, figus_paquete)] = 1
+        #figus_paquete += 1
         paquetes += 1
     #print (album)    
     #print(esta_lleno(album)) 
-    return paquetes
+    return paquetes 
 
     
-
-#lista = [1,2,3,4,5,6,7,8,9,10]
+lista = [1,2,3,4,5,6,7,8,9,10]
 def funcion_promedio(lista):
     promedio = sum(lista)/len(lista) 
     print(promedio)
 
-print(cuantos_paquetes(figus_total, figus_paquete))
+#print(cuantos_paquetes(figus_total, figus_paquete))
 
 def funcion_4 ():
     promedios = []
