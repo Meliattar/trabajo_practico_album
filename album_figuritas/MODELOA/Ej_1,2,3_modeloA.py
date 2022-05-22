1315b1799fe412716f683d6f27b9cabba5b47cb7
Meliattar <meliattarcohen@gmail.com>
2022-05-22 19:52:24 -0300

    Falta el 5

--- album_figuritas/MODELOA/Ej_1,2,3_modeloA.py
+++ album_figuritas/MODELOA/Ej_1,2,3_modeloA.py
@@ -1,18 +1,15 @@
 import numpy as np 
 import random 
 
-figus_paquete = 0
-figus_total = int(input("Ingrese cuantas figuritas tiene su album en total: "))
-album = [0]*figus_total
-
+figus_paquete = 5
+figus_total = (int(input("Ingrese la cantidad total de figuritas de su album: ")))
 def generar_paquete(figus_total, figus_paquete):
-    while figus_paquete <= 5: 
-        figurita_obtenida = random.randint(0,figus_total-1)
-        #print(figurita_obtenida)
-        figus_paquete += 1
-        print(figurita_obtenida)
-        return figurita_obtenida
-generar_paquete(figus_total,figus_paquete)
+    i = 0
+    paquetes = []
+    while i < figus_paquete: 
+        paquetes.append(random.randint(0,figus_total-1))
+        i +=1
+    return paquetes
 
 def esta_lleno(album):
     if 0 in album:
@@ -20,14 +17,15 @@ def esta_lleno(album):
     return True
 
 def cuantos_paquetes(figus_total, figus_paquete):
+    album = [0]*figus_total
     paquetes = 0
     while esta_lleno(album) == False:
-        album[generar_paquete(figus_total, figus_paquete)] = 1
         paquetes += 1
-        print (album)    
-        print(esta_lleno(album))
-    return paquetes
-print(cuantos_paquetes(figus_total, figus_paquete))
-
+        paquete = generar_paquete(figus_total, figus_paquete)
+        for indice in paquete:
+            album[indice]=1
+    print (album)    
+    return paquetes 
+print(cuantos_paquetes(figus_total,figus_paquete))
 
     
